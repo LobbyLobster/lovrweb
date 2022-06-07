0a2d5db411561d61c3308d8d9851bd8048f48d42
Tarik <humblerogue33@gmail.com>
2022-06-08 01:09:37 +0300

    second

--- static/demo/main.lua
+++ static/demo/main.lua
@@ -1,9 +1,14 @@
 function lovr.load()
-	skybox = lovr.graphics.newTexture('pano1.jpeg')
-	-- skybox = lovr.graphics.newTexture('pano2.jpeg')
-	-- skybox = lovr.graphics.newTexture('pano3.jpeg')
+	skybox0 = lovr.graphics.newTexture('pano1.jpeg', { mipmaps = false })
+	skybox1 = lovr.graphics.newTexture('pano2.jpeg', { mipmaps = false })
+	skybox2 = lovr.graphics.newTexture('pano3.jpeg', { mipmaps = false })
+
+	material = lovr.graphics.newMaterial(skybox1)
+	
 end
 
 function lovr.draw()
-	lovr.graphics.skybox(skybox)
+	local angle = lovr.timer.getTime()
+	lovr.graphics.sphere(material, 0, 1.7, -1, .1, angle)
+	lovr.graphics.skybox(skybox0)
 end
